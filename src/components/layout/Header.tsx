8f30c3146e6b845086b967fc999b04eb2a5ab344
brian djerf <brian@eaiti.com>
2026-03-16 16:24:42 -0400

    change bg image

--- src/components/layout/Header.tsx
+++ src/components/layout/Header.tsx
@@ -5,22 +5,25 @@ import NameWhite from '../../assets/name_white.svg';
 
 export default function Header() {
     return (
-        <header className="h-screen bg-[url('/src/assets/golf_swing.png')] bg-cover lg:bg-contain bg-center bg-no-repeat flex flex-col">
-            <nav className="container px-4 py-4 flex justify-center mx-auto">
-                <Link to="/">
-                    <img src={TransparentLogo} alt="Quincy Golf Lounge" className="h-12 w-auto" />
-                </Link>
-            </nav>
-            <div className="flex-1 flex flex-col items-center justify-center gap-4">
-                <img src={NameWhite} alt="Quincy Golf Lounge" className="h-24 lg:h-32 w-auto" />
-                <a
-                    href="https://www.yourgolfbooking.com/venues/quincy-golf-lounge/booking"
-                    target="_blank"
-                    rel="noopener noreferrer"
-                    className="block bg-brand-green text-brand-cream text-xl lg:text-2xl font-main uppercase px-4 py-2"
-                >
-                    Book A Bay
-                </a>
+        <header className="relative h-screen bg-[url('/src/assets/birmingham-mail.png')] bg-cover lg:bg-contain bg-center bg-no-repeat">
+            <div className="absolute inset-0 bg-brand-black/40 backdrop-blur-[1px]"></div>
+            <div className="relative z-10 flex flex-col h-full">
+                <nav className="px-4 py-4 flex justify-end mx-auto">
+                    <Link to="/">
+                        <img src={TransparentLogo} alt="Quincy Golf Lounge" className="h-12 w-auto" />
+                    </Link>
+                </nav>
+                <div className="flex-1 flex flex-col items-center justify-center gap-4">
+                    <img src={NameWhite} alt="Quincy Golf Lounge" className="h-24 lg:h-32 w-auto" />
+                    <a
+                        href="https://www.yourgolfbooking.com/venues/quincy-golf-lounge/booking"
+                        target="_blank"
+                        rel="noopener noreferrer"
+                        className="block bg-brand-green text-brand-cream text-xl lg:text-2xl font-main uppercase px-4 py-2"
+                    >
+                        Book A Bay
+                    </a>
+                </div>
             </div>
         </header>
     );
